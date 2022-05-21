67544e1d307d71949d059206efebc9bbad82e453
Abraham Mehary <abrahammehary@Abrahams-MacBook-Pro.local>
2022-05-21 15:01:36 +0100

    Completed navbar component

---
+++ src/components/home/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './HomeStyles.css';
+import GymFloor from '../../assets/luis-vidal-FodEsaNZs48-unsplash.jpg';
+
+const Home = () => {
+	return (
+		<div className="home">
+			<img src={GymFloor} alt="" />
+			<div className="container">
+				<div className="content">
+					<div>
+						<h4>Get Your First Month For</h4>
+						<h1>Only £10</h1>
+						<p>No Joining Fee | Limited Time Only</p>
+						<button>Join Now</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Home;
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import './NavbarStyles.css';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { IoIosFitness } from 'react-icons/io';
+import Logo from '../../assets/fitness_active.png';
+import { AiOutlineClose } from 'react-icons/ai';
+
+const Navbar = () => {
+	const [ nav, setNav ] = useState(false);
+	const handleNav = () => setNav(!nav);
+	return (
+		<div className="navbar">
+			<div className="container">
+				<div className="logo">
+					<IoIosFitness className="main-icon" />
+					<h2>Fitness active</h2>
+				</div>
+				<ul className="nav-menu">
+					<li>Home</li>
+					<li>Benefits</li>
+					<li>Clients</li>
+					<li>Purchase</li>
+					<li>Contact</li>
+				</ul>
+				<div className="hamburger" onClick={handleNav}>
+					{nav ? <AiOutlineClose className="icon" /> : <GiHamburgerMenu className="icon" />}
+				</div>
+				<div className={nav ? 'mobile-menu active' : 'mobile-menu'}>
+					<ul className="mobile-nav">
+						<li>Home</li>
+						<li>Benefits</li>
+						<li>Clients</li>
+						<li>Purchase</li>
+						<li>Contact</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Navbar;
